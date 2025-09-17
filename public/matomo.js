4fa117b69fa9d1fe4560c6f4b095f0ed07dafabc
gniewkob <gniewko74@gmail.com>
2025-09-17 14:04:46 +0200

    chore(analytics): update Matomo tracker snippet for gliwicka111.pl (siteId=591)

--- public/matomo.js
+++ public/matomo.js
@@ -1,17 +1,11 @@
-var _paq = (window._paq = window._paq || []);
-_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-_paq.push(["setCookieDomain", "*.gliwicka111.pl"]);
-_paq.push(["setDomains", ["*.gliwicka111.pl"]]);
-_paq.push(["trackPageView"]);
-_paq.push(["enableLinkTracking"]);
-(function () {
-  var u = "//stats0.mydevil.net/";
-  _paq.push(["setTrackerUrl", u + "matomo.php"]);
-  _paq.push(["setSiteId", "589"]);
-  var d = document,
-    g = d.createElement("script"),
-    s = d.getElementsByTagName("script")[0];
-  g.async = true;
-  g.src = u + "matomo.js";
-  s.parentNode.insertBefore(g, s);
+var _paq = window._paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//stats0.mydevil.net/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '591']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
