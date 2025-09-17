6d4b0e329289f77105ed3e2bb33e9caf26ffa400
gniewkob <gniewko74@gmail.com>
2025-09-17 14:42:52 +0200

    fix(analytics): configure Matomo before tracking; keep GET + https + siteId=591

--- public/matomo.js
+++ public/matomo.js
@@ -2,13 +2,12 @@ var _paq = window._paq = window._paq || [];
 // Force GET to avoid cross-origin POST/beacon being rejected
 _paq.push(['disableAlwaysUseSendBeacon']);
 _paq.push(['setRequestMethod', 'GET']);
-// Track after configuring
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
 (function() {
   var u = 'https://stats0.mydevil.net/';
   _paq.push(['setTrackerUrl', u + 'matomo.php']);
   _paq.push(['setSiteId', '591']);
+  _paq.push(['enableLinkTracking']);
+  _paq.push(['trackPageView']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
