0795cfce187097e9166bb99d7e22025996161e21
gniewkob <gniewko74@gmail.com>
2025-09-17 14:15:48 +0200

    fix(analytics): force GET for Matomo requests and use https (siteId=591)

--- public/matomo.js
+++ public/matomo.js
@@ -1,10 +1,13 @@
 var _paq = window._paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+// Force GET to avoid cross-origin POST/beacon being rejected
+_paq.push(['disableAlwaysUseSendBeacon']);
+_paq.push(['setRequestMethod', 'GET']);
+// Track after configuring
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//stats0.mydevil.net/";
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  var u = 'https://stats0.mydevil.net/';
+  _paq.push(['setTrackerUrl', u + 'matomo.php']);
   _paq.push(['setSiteId', '591']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
